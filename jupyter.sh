208b3640d6d33ccf23b9539a85c15515a020c7e5
javad-rezaie <rezaie.j@gmail.com>
2024-03-09 15:16:05 +0100

    jupyter.sh bash script are created to create a container and run Jupyter on top.

---
+++ jupyter.sh
@@ -0,0 +1,17 @@
+DATA_DIR="/mnt/SSD2/kvasir-instrument/"
+OUT_DIR="$PWD/out"
+CONFIG_DIR=$PWD/codes/
+NOTEBOOK_DIR=$PWD/notebooks/
+
+GPUS=3
+
+docker run -it --rm \
+    --gpus all \
+    --mount type=bind,source=$CONFIG_DIR,target=/configs \
+    --mount type=bind,source=$DATA_DIR,target=/data \
+    --mount type=bind,source=$OUT_DIR,target=/out \
+    --mount type=bind,source=$NOTEBOOK_DIR,target=/notebooks \
+    --shm-size 8g \
+    -p 8888:8888 \
+    mmdetection:latest \
+    jupyter-lab  --ip 0.0.0.0 --port 8888 --allow-root /notebooks
